64ecd83dbc3b57eeab24100256914849f06c8614
Siratox <siratox5@gmail.com>
2025-05-21 18:59:17 +0200

    fix: fix prettier setup and add config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+import config from './config/environment.js';
+
+dotenv.config();
+
+(async () => {
+  // Replace with your SOCKS proxy
+  const proxy = process.env.TOR_PROXY_URI;
+
+  const browser = await puppeteer.launch({
+    headless: false, // or true
+    args: [`--proxy-server=${proxy}`],
+  });
+
+  const page = await browser.newPage();
+
+  // Optional: verify the IP to make sure the proxy is working
+  await page.goto(config.WEBSITE_URL);
+
+  //   await browser.close();
+})();
